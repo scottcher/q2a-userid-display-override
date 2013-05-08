aa9d917257ebbe92eaea0d6577c657a62f596dce
scottcher <scott.cherkofsky@gmail.com>
2013-05-08 13:53:27 -0300

    Update qa-userid-display-overrides.php
    Simplified the code and fixed a _potential_ bug in the code.

--- qa-userid-display-overrides.php
+++ qa-userid-display-overrides.php
@@ -1,18 +1,18 @@
 <?php
 
   	// override for qa-app-users.php qa_get_one_user_html() function
-		//
-		function qa_get_one_user_html($handle, $microformats)
-		{
-			$userprofiles = qa_db_select_with_pending(qa_db_user_profile_selectspec($handle,false));
-			if(@$userprofiles['name'] == '') {
-				return strlen($handle) ? ('<A HREF="'.qa_path_html('user/'.$handle).
-					'" CLASS="qa-user-link'.($microformats ? ' url nickname' : '').'">'.qa_html($handle).'</A>') : '';
-			} else {
-				return strlen($handle) ? ('<A HREF="'.qa_path_html('user/'.$handle).
-					'" CLASS="qa-user-link'.($microformats ? ' url nickname' : '').'">'.qa_html($userprofiles['name']).'</A>') : '';
-			}
+	//
+	function qa_get_one_user_html($handle, $microformats)
+	{
+		$userprofiles = qa_db_select_with_pending(qa_db_user_profile_selectspec($handle,false));
+		$userdisplayhandle = $handle;
+		if(@$userprofiles['name'] != '') {
+			$userdisplayhandle = @$userprofiles['name'];
 		}
+		return strlen($userdisplayhandle) ? ('<A HREF="'.qa_path_html('user/'.$handle).
+			'" CLASS="qa-user-link'.($microformats ? ' url nickname' : '').
+			'">'.qa_html($userdisplayhandle).'</A>') : '';
+	}
 
 /*
 	Omit PHP closing tag to help avoid accidental output
